e16fc6b9b676e347efe61d93d94c387b9aa06430
Noah Gift <noah.gift@gmail.com>
2021-08-08 10:10:17 -0400

    Update test_change.py

--- test_change.py
+++ test_change.py
@@ -8,30 +8,35 @@ import change
 class TestChange(unittest.TestCase):
     def test_get_quarter(self):
         c = change.Change(.25)
-        quarter, qrem, dime, drem, nickel, nrem, penny = c.make_change_conditional()
+        quarter, qrem, dime, drem, nickel, nrem, penny =\
+            c.make_change_conditional()
         self.assertEqual(quarter,1)  #quarters
         self.assertEqual(qrem, 0)   #quarter remainder
     def test_get_dime(self):
         c = change.Change(.20)
-        quarter, qrem, dime, drem, nickel, nrem, penny = c.make_change_conditional()
+        quarter, qrem, dime, drem, nickel, nrem, penny =\
+            c.make_change_conditional()
         self.assertEqual(quarter,0)  #quarters
         self.assertEqual(qrem, 20)   #quarter remainder
         self.assertEqual(dime, 2)    #dime
         self.assertEqual(drem, 0)    #dime remainder
     def test_get_nickel(self):
         c = change.Change(.05)
-        quarter, qrem, dime, drem, nickel, nrem, penny = c.make_change_conditional()
+        quarter, qrem, dime, drem, nickel, nrem, penny =\
+            c.make_change_conditional()
         self.assertEqual(dime, 0)    #dime
         self.assertEqual(drem, 0)    #dime remainder
         self.assertEqual(nickel, 1)  #nickel
         self.assertEqual(nrem, 0)    #nickel remainder
     def test_get_penny(self):
         c = change.Change(.04)
-        quarter, qrem, dime, drem, nickel, nrem, penny = c.make_change_conditional()
+        quarter, qrem, dime, drem, nickel, nrem, penny =\
+            c.make_change_conditional()
         self.assertEqual(penny, 4)  #nickel
     def test_small_number(self):
         c = change.Change(.0001)
-        quarter, qrem, dime, drem, nickel, nrem, penny = c.make_change_conditional()
+        quarter, qrem, dime, drem, nickel, nrem, penny =\
+            c.make_change_conditional()
         self.assertEqual(quarter,0)  #quarters
         self.assertEqual(qrem, 0)   #quarter remainder
         self.assertEqual(dime, 0)    #dime
@@ -48,7 +53,8 @@ class TestChange(unittest.TestCase):
         self.assertEqual(drem, 0)  #nickel
     def test_get_quarter_dime_penny(self):
         c = change.Change(.86)
-        quarter, qrem, dime, drem, nickel, nrem, penny = c.make_change_conditional()
+        quarter, qrem, dime, drem, nickel, nrem, penny =\
+            c.make_change_conditional()
         self.assertEqual(quarter,3)  #quarters
         self.assertEqual(qrem, 11)   #quarter remainder
         self.assertEqual(dime, 1)    #dime
@@ -56,7 +62,8 @@ class TestChange(unittest.TestCase):
         self.assertEqual(penny, 1)   #penny
     def test_get_quarter_dime_nickel_penny(self):
         c = change.Change(.91)
-        quarter, qrem, dime, drem, nickel, nrem, penny = c.make_change_conditional()
+        quarter, qrem, dime, drem, nickel, nrem, penny =\
+            c.make_change_conditional()
         self.assertEqual(quarter,3)  #quarters
         self.assertEqual(qrem, 16)   #quarter remainder
         self.assertEqual(dime, 1)    #dime
